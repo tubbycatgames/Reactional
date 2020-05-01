e8751687825bbc8d0d9d2de9daec132ad4441d36
Phil Simmons <phil@tubbycatgames.com>
2020-05-01 00:04:02 -0400

    Added Basic Swipe Handler

--- src/screens/swipe.tsx
+++ src/screens/swipe.tsx
@@ -1,13 +1,47 @@
 import React from 'react'
-import { ScreenView } from './view'
+import {
+  Directions,
+  FlingGestureHandler,
+  State,
+} from 'react-native-gesture-handler'
+import { StyleSheet, View, Alert } from 'react-native'
+
 import { MenuButton } from '../components/buttons'
+
+import { ScreenView } from './view'
 
 const SwipeScreen = ({ navigation }: any) => {
   return (
     <ScreenView>
+      <FlingGestureHandler
+        direction={Directions.LEFT}
+        onHandlerStateChange={({ nativeEvent }) => {
+          if (nativeEvent.state === State.ACTIVE) {
+            Alert.alert('flingged left!')
+          }
+        }}
+      >
+        <FlingGestureHandler
+          direction={Directions.RIGHT}
+          onHandlerStateChange={({ nativeEvent }) => {
+            if (nativeEvent.state === State.ACTIVE) {
+              Alert.alert('flingged right!')
+            }
+          }}
+        >
+          <View style={styles.container} />
+        </FlingGestureHandler>
+      </FlingGestureHandler>
       <MenuButton navigate={navigation.navigate} />
     </ScreenView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100%',
+  },
+})
 
 export default SwipeScreen
