64c1a11a8f3e2d47ee0f49a2560ca24fb0746deb
Phil Simmons <phil@tubbycatgames.com>
2020-05-28 15:03:56 -0400

    Built out Basic Swipe Board Game

--- src/screens/game.tsx
+++ src/screens/game.tsx
@@ -1,13 +1,76 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { StyleSheet, View } from 'react-native'
 
+import { ResponseType, prompt } from '../utils/prompt'
 import { SchemeText } from '../components/schemed'
+import { SwipeBoard } from '../components/boards'
 
 import { ScreenView } from './view'
 
-const GameScreen = () => (
-  <ScreenView>
-    <SchemeText>In Progress!</SchemeText>
-  </ScreenView>
-)
+interface Props {
+  goal: string
+  timeout: number
+}
 
-export default GameScreen
+interface State {
+  resolve: Function
+  time: number
+  type: ResponseType
+}
+
+export default class GameScreen extends Component<Props, State> {
+  static defaultProps = {
+    goal: 'up',
+    timeout: 10000,
+  }
+
+  state = {
+    resolve: (direction: string) => {},
+    time: -1,
+    type: -1,
+  }
+
+  resolvePrompt = (direction: string) => () => {
+    this.state.resolve(direction)
+  }
+
+  async componentDidMount() {
+    const testPrompt = await prompt(
+      new Promise((resolve) => {
+        this.setState({ resolve })
+      }),
+      this.props.goal,
+      this.props.timeout
+    )
+
+    this.setState({
+      time: testPrompt.time,
+      type: testPrompt.type,
+    })
+  }
+
+  render() {
+    return (
+      <ScreenView>
+        <SwipeBoard
+          onDown={this.resolvePrompt('down')}
+          onLeft={this.resolvePrompt('left')}
+          onRight={this.resolvePrompt('right')}
+          onUp={this.resolvePrompt('up')}
+        >
+          <View style={styles.container}>
+            <SchemeText>Type: {this.state.type}</SchemeText>
+            <SchemeText>Time: {this.state.time}</SchemeText>
+          </View>
+        </SwipeBoard>
+      </ScreenView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: '100%',
+  },
+})
